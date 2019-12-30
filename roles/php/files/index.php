24a418c661491f78faf053ca899348f9e7234db2
adekunleso <adekunleso@yahoo.com>
2019-12-30 15:25:00 +0000

    adding roles and lamp.yml file

---
+++ roles/php/files/index.php
@@ -0,0 +1,11 @@
+<!DOCTYPE html>
+<html>
+<head>
+  <title>Welcome to DevOps on AWS</title>
+</head>
+<body>
+   <h1><?php echo "Welcome on AWS. This text was generated using PHP 7" ?></h1> 
+   </body>
+   </html>
+   
+   
