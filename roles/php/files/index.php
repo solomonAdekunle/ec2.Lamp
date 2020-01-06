57f0d926836756ad9cc8c0b04d95a68c864ebd65
adekunleso <adekunleso@yahoo.com>
2020-01-06 21:47:17 +0000

    texts added to php filse

--- roles/php/files/index.php
+++ roles/php/files/index.php
@@ -4,9 +4,9 @@
   <title>Welcome to DevOps on AWS</title>
 </head>
 <body>
-<h1><?php echo "Welcome on AWS. This text was generated using PHP 7" ?></h1>
-<h2><?php echo "this text is my second devops text" ?></h2> 
-   </body>
-   </html>
+ <h1><?php echo "Welcome on AWS. This text was generated using PHP 7" ?></h1>
+ <h2><?php echo "this text is my second devops text" ?></h2> 
+ </body>
+ </html>
    
    
